c200ffdb687c066bc4f1c7903a6a6d8b1ac3875a
didierganthier <didierganthierperan@gmail.com>
2021-04-20 15:35:31 -0400

    CustomClipper DrawClip added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -19,14 +20,51 @@ class LoginPage extends StatelessWidget {
           ),
           child: Column(
             children: [
-              SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+              ),
               Stack(
                 children: [
                   ClipPath(
+                    clipper: DrawClip(),
                     child: Container(
                       height: 560,
                       width: size.width - 50,
                       color: Colors.white,
+                    ),
+                  ),
+                  Positioned(
+                    right: 5,
+                    child: Container(
+                      child: Image.asset(
+                        "assets/blood_icon.jpg",
+                        width: 60,
+                        height: 60,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 10,
+                    top: 110,
+                    child: Text(
+                      "Login",
+                      style: GoogleFonts.montserrat(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        shadows: [
+                          Shadow(
+                            color: Colors.grey.shade300,
+                            offset: Offset(3.0, 3.0),
+                            blurRadius: 3.0,
+                          ),
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(-3.0, 3.0),
+                            blurRadius: 3.0,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
@@ -36,5 +74,26 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class DrawClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, 50);
+    path.quadraticBezierTo(0, 0, 50, 30);
+    path.lineTo(size.width, size.height / 3);
+    path.lineTo(size.width, size.height - 50);
+    path.quadraticBezierTo(
+        size.width, size.height, size.width - 50, size.height - 30);
+    path.lineTo(0, size.height * 2 / 3);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
   }
 }
